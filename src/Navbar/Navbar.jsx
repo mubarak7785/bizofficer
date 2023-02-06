65fd6662172d563e51ce45b914495f07e692f89c
mubarak7785 <mubarakjinabade07@gmail.com>
2023-02-06 16:35:20 +0530

    done with navbar

---
+++ src/Navbar/Navbar.jsx
@@ -0,0 +1,18 @@
+import "./Navbar.css"
+import { MdMenu,MdOutlineLogin } from 'react-icons/Md';
+
+
+export const Navbar=()=>{
+    return(
+        <div className="main">
+            <div className="menu">
+                <p className="menu-btn"> <MdMenu/></p>
+                <div className="img_div"><img src="https://web.bizofficer.com/subscription/icon-logo.png" height="100%" width="100%" alt="" /></div>
+                <p className="cas">Cases</p>
+            </div>
+            <div>
+                <p className="login"><MdOutlineLogin/></p>
+            </div>
+        </div>
+    )
+}
