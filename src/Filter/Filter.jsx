5e966d1a1d4cc1e084747abfe1ed01657d1f483d
mubarak7785 <mubarakjinabade07@gmail.com>
2023-02-06 17:05:02 +0530

    updated css

--- src/Filter/Filter.jsx
+++ src/Filter/Filter.jsx
@@ -3,10 +3,21 @@ import "./Filter.css"
 export const Filter=()=>{
     return(<div>
         <div className="main-con">
-            <select name="" id=""></select>
-            <select name="" id=""></select>
-            <select name="" id=""></select>
-            <select name="" id=""></select>
+            <select name="" id="">
+                <option value="">All Channels</option>
+                <option value="">Primary</option>
+                <option value="">Secondary</option>
+            </select>
+            <select name="" id="">
+                <option value="">All Case Type</option>
+            </select>
+            <select name="" id="">
+            <option value="">All Status</option>
+            </select>
+            <select name="" id="">
+                <option value="">All Cases</option>
+            </select>
+            <input type="text" placeholder="Search"/>
         </div>
     </div>)
 }
